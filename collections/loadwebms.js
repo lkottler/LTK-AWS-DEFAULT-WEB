ef0459b8ea62d81df8ae4c5b613daa3441ed5d04
Logan Kottler <lkottler@wisc.edu>
2022-01-20 19:34:33 -0600

    Added some content

--- collections/loadwebms.js
+++ collections/loadwebms.js
@@ -1,27 +1,13 @@
+var total_videos = 2;
 
-var dir = "/webms/";
-var fileextension = ".webm";
-$.ajax({
-    //This will retrieve the contents of the folder if the folder is configured as 'browsable'
-    url: dir,
-    success: function (data) {
-        //List all .png file names in the page
-        $(data).find("a:contains(" + fileextension + ")").each(function () {
-            var filename = this.href.replace(window.location.host, "").replace("http://", "");
-            $("body").append("<video src='" + dir + filename + "'>");
-        });
-    }
-});
-/*
-var video = document.createElement('video');
+for (let i = 1; i < (total_videos + 1); i++) {
+	var video = document.createElement('video');
 
-video.src = "video/webm";
-video.src = "webms/hype.webm";
-video.autoplay = true;
+	video.src = "video/webm";
+	video.src = "webms/" + i + ".webm";
 
-
-video.setAttribute("width", "320");
-video.setAttribute("height", "240");
-video.setAttribute("controls", "controls");
-document.body.appendChild(video);
-*/
+	video.setAttribute("width", "400");
+	video.setAttribute("height", "300");
+	video.setAttribute("controls", "controls");
+	document.body.appendChild(video);
+}
