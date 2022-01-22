65394b5c042c1ea1848233c733b8c2dfb991bbc9
Logan Kottler <lkottler@wisc.edu>
2022-01-21 23:27:13 -0600

    Added more webms

--- collections/loadwebms.js
+++ collections/loadwebms.js
@@ -1,15 +1,15 @@
 var categories = [
-	{"folder": "trollface", "count": 20},
-	{"folder": "terry", "count": 6},
-	{"folder": "feels", "count": 40},
-	{"folder": "funny", "count": 62},
-	{"folder": "kino", "count": 15}
+	{"folder": "trollface", "count": 21},
+	{"folder": "terry", "count": 8},
+	{"folder": "feels", "count": 41},
+	{"folder": "funny", "count": 71},
+	{"folder": "kino", "count": 16}
 	];
 
 var category = "trollface";
 var count = categories[categories.findIndex(x => x.folder === category)].count;
 
-function loadVideos(new_category){
+function loadVideos(new_category){ 
 	console.log(new_category);
 	destroyVideos();
 	category = new_category;
